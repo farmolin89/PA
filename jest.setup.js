e1f9543acc16b808b8c56d1c55b1b84dceac28da
farmolin89 <farmolin.zero@gmail.com>
2025-11-07 15:20:00 +0500

    Delete jest.setup.js

--- jest.setup.js
+++
@@ -1,7 +0,0 @@
-process.env.NODE_ENV = 'test';
-if (!process.env.SESSION_SECRET) {
-  process.env.SESSION_SECRET = 'test-session-secret';
-}
-if (!process.env.CSRF_SECRET) {
-  process.env.CSRF_SECRET = '12345678901234567890123456789012';
-}
